eee6916f961cbfdda309a6a7e00640daf9d1d363
Simon J Mudd <simon.mudd@booking.com>
2016-11-12 15:18:14 +0100

    go/inst/instance_topology.go: fix tiny typo in comment

--- go/inst/instance_topology.go
+++ go/inst/instance_topology.go
@@ -28,8 +28,8 @@ import (
 	"github.com/outbrain/orchestrator/go/config"
 )
 
-// getASCIITopologyEntry will get an ascii topology tree rooted at given instance. Ir recursively
-// draws the tree
+// getASCIITopologyEntry will get an ascii topology tree rooted at given
+// instance. It recursively draws the tree.
 func getASCIITopologyEntry(depth int, instance *Instance, replicationMap map[*Instance]([]*Instance), extendedOutput bool) []string {
 	if instance == nil {
 		return []string{}
